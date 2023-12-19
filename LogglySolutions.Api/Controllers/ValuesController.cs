0d887dee4034dd32e429b6dce6627b45e265accb
maheshDivekar <42762536+maheshDivekar@users.noreply.github.com>
2023-12-20 00:24:42 +0530

    Add project files.

---
+++ LogglySolutions.Api/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace LogglySolutions.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogInformation("Testing Microsoft Information");
+
+            try
+            {
+                var a = 6;
+                var b=0;
+                var c = 6 / b;
+                return new[] { "value1", "value2" };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+         //  Log.Information("Log: Log.Information seri");
+            //Log.Warning("Log: Log.Warning MD ");
+            //Log.Error("Log: Log.Error Megha");
+            //Log.Fatal("Log: Log.Fatal Divekar");
+
+           
+        }
+    }
+}
---
+++ LogglySolutions.Api/Program.cs
@@ -0,0 +1,72 @@
+﻿using Loggly;
+using Loggly.Config;
+using LogglySolutions.Api.Settings;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.IO;
+
+namespace LogglySolutions.Api
+{
+    public class Program
+    {
+        private static string _environmentName;
+
+        public static void Main(string[] args)
+        {
+            var webHost = BuildWebHost(args);
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{_environmentName}.json", optional: true, reloadOnChange: true)
+                        .Build();
+
+            var logglySettings = new LogglySettings();
+            configuration.GetSection("Serilog:Loggly").Bind(logglySettings);
+
+            SetupLogglyConfiguration(logglySettings);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, config) =>
+                {
+                    config.ClearProviders();
+                    _environmentName = hostingContext.HostingEnvironment.EnvironmentName;
+                    config.AddSerilog();
+
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+        private static void SetupLogglyConfiguration(LogglySettings logglySettings)
+        {
+            //Configure Loggly
+            var config = LogglyConfig.Instance;
+            config.CustomerToken = logglySettings.CustomerToken;
+            config.ApplicationName = logglySettings.ApplicationName;
+            config.Transport = new TransportConfiguration()
+            {
+                EndpointHostname = logglySettings.EndpointHostname,
+                EndpointPort = logglySettings.EndpointPort,
+                LogTransport = logglySettings.LogTransport
+            };
+            config.ThrowExceptions = logglySettings.ThrowExceptions;
+
+            //Define Tags sent to Loggly
+            config.TagConfig.Tags.AddRange(new ITag[]{
+                new ApplicationNameTag {Formatter = "Application-{0}"},
+                new HostnameTag { Formatter = "Host-{0}" }
+            });
+        }
+    }
+}
